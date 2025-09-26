b2b64e5cd34c2ed468d8ae5f4481e59ed86a093e
inazrabuu <inazrabuu@gmail.com>
2025-09-26 14:10:41 +0700

    cookie parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,8 @@ async function bootstrap() {
     credentials: true,
   });
 
-  app.use(bodyParser.text({ type: 'text/plain' }))
+  app.use(bodyParser.text({ type: 'text/plain' }));
+  app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000);
 }
